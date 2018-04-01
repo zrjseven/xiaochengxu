0a0f0dab29f3de207fe9e371cd24ebaa2a9d06ba
zrjseven <455032215@qq.com>
2018-04-01 18:20:46 +0800

    first

---
+++ data/Home_data.js
@@ -0,0 +1,234 @@
+var Home_data=[
+  {
+    id:0,
+    title:"空调维修",
+    titleTwo:"维修中央空调及家用空调",
+    price: "200",
+    value:"1",
+    image:"https://img12.360buyimg.com//n0/jfs/t16483/136/2605663072/62865/47d8878c/5ab8ff94N0f6f7b35.jpg",
+    Imgone: "https://img12.360buyimg.com//n0/jfs/t16483/136/2605663072/62865/47d8878c/5ab8ff94N0f6f7b35.jpg",
+    Imgtwo:"https://img12.360buyimg.com//n0/jfs/t16483/136/2605663072/62865/47d8878c/5ab8ff94N0f6f7b35.jpg",
+    selected:true,
+    // img:[
+    //   {name:55},
+    //   {name:666}
+    // ]
+    img:[
+      /*{ image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20171115/20171115110719510981_original.jpg", mode: 'widthFix'},
+      { image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20161219/20161219093807784269_original.jpg", mode: 'widthFix'},
+      { image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20161219/20161219093810349285_original.jpg", mode: 'widthFix'},
+      { image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20161219/20161219093813167921_original.jpg", mode: 'widthFix' },
+      { image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20161219/20161219093814935600_original.jpg", mode: 'widthFix' },
+      { image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20161219/20161219093817527717_original.jpg", mode: 'widthFix' },
+      { image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20161219/20161219093822750588_original.jpg", mode: 'widthFix' },
+      { image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20161219/20161219093824598774_original.jpg", mode: 'widthFix' },
+      { image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20161219/20161219093827290232_original.jpg", mode: 'widthFix' },
+      { image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20161219/20161219093829298284_original.jpg", mode: 'widthFix' },
+      { image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20161219/2016121909383925061_original.jpg", mode: 'widthFix' },
+      { image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20161219/2016121909383925061_original.jpg", mode: 'widthFix' },
+      { image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20170629/2017062914535078223_original.jpg", mode: 'widthFix' },*/
+    ]
+  },
+  {
+    id: 1,
+    title: "电视维修",
+    titleTwo: "液晶电视，等离子电视，LED电视",
+    price: "150",
+    value: "1",
+    image: "https://img12.360buyimg.com/n1/s450x450_jfs/t17707/10/1130560108/361252/2fb1eb5/5abdedf7N509a9378.jpg",
+    Imgone:"https:////img12.360buyimg.com/n1/s450x450_jfs/t17707/10/1130560108/361252/2fb1eb5/5abdedf7N509a9378.jpg",
+    Imgtwo: "https:////img12.360buyimg.com/n1/s450x450_jfs/t17707/10/1130560108/361252/2fb1eb5/5abdedf7N509a9378.jpg",
+    selected: true,
+    img:[
+      /*{
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20171031/20171031232228644662_original.jpg",
+        mode: 'widthFix'
+      },
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20171031/20171031232229235145_original.jpg",
+         mode: 'widthFix'
+      },
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20171102/20171102143039424294_original.jpg",
+        mode: 'widthFix'
+      },
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20171031/20171031232243781406_original.jpg",
+        mode: 'widthFix'
+      },
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20171031/20171031232353254161_original.jpg",
+        mode: 'widthFix'
+      },
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20171101/20171101191141708950_original.jpg",
+        mode: 'widthFix'
+      },
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20171031/20171031232409516746_original.jpg",
+        mode: 'widthFix'
+      },
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20171031/20171031232409523196_original.jpg",
+        mode: 'widthFix'
+      },
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20171031/20171031232442377774_original.jpg",
+        mode: 'widthFix'
+      },
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20171031/20171031232442208606_original.jpg",
+        mode: 'widthFix'
+      },
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20171031/2017103123244195095_original.jpg",
+        mode: 'widthFix'
+      },
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20171031/20171031232457784713_original.jpg",
+        mode: 'widthFix'
+      },
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20171031/20171031232457762898_original.jpg",
+        mode: 'widthFix'
+      },
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20171031/20171031232456912178_original.jpg",
+        mode: 'widthFix'
+      },
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20171031/20171031232519457796_original.jpg",
+        mode: 'widthFix'
+      },
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20171031/2017103123251879781_original.jpg",
+        mode: 'widthFix'
+      }*/
+    ]
+  },
+  {
+    id: 2,
+    title: "冰箱维修",
+    titleTwo: "电冰箱，电冰柜，制冷剂维修",
+    price: "200",
+    value: "1",
+    image: "https:////img14.360buyimg.com/n1/s450x450_jfs/t11269/115/1716760449/85200/5990c79d/5a0687baN912b4dcd.jpg",
+    Imgone: "https:////img14.360buyimg.com/n1/s450x450_jfs/t11269/115/1716760449/85200/5990c79d/5a0687baN912b4dcd.jpg",
+    Imgtwo: "https:////img14.360buyimg.com/n1/s450x450_jfs/t11269/115/1716760449/85200/5990c79d/5a0687baN912b4dcd.jpg",
+    selected: true,
+    img: [
+      /*{
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20171130/20171130145223121421_original.jpg",
+        mode: 'widthFix'
+      },
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20171130/2017113014535474183_original.jpg",
+        mode: 'widthFix'
+      },
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20171130/20171130145352967966_original.jpg",
+        mode: 'widthFix'
+      },
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20171130/20171130145356762340_original.jpg",
+        mode: 'widthFix'
+      },
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20171206/20171206092239616607_original.jpg",
+        mode: 'widthFix'
+      },
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20171130/20171130145221653224_original.jpg",
+        mode: 'widthFix'
+      }*/
+    ]
+  },
+  {
+    id: 3,
+    title: "洗衣机维修",
+    titleTwo: "洗衣机维修",
+    price: "200",
+    value: "1",
+    image: "https://img10.360buyimg.com/n1/s450x450_jfs/t2680/300/3085318076/201501/bbee60ec/577dd103N67caec65.jpg",
+    Imgone: "https://img10.360buyimg.com/n1/s450x450_jfs/t2680/300/3085318076/201501/bbee60ec/577dd103N67caec65.jpg",
+    Imgtwo: "https://img10.360buyimg.com/n1/s450x450_jfs/t2680/300/3085318076/201501/bbee60ec/577dd103N67caec65.jpg",
+    selected: true,
+    img: [
+      /*{
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20171115/20171115105005767122_original.jpg",
+        mode: 'widthFix'
+      },
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20171116/20171116155420650567_original.jpg",
+        mode: 'widthFix'
+      },
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20170323/20170323155103829614_original.jpg",
+        mode: 'widthFix'
+      },
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20170323/20170323155114886378_original.jpg",
+        mode: 'widthFix'
+      },
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20170323/20170323155120205409_original.jpg",
+        mode: 'widthFix'
+      },
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20170329/20170329154926638158_original.jpg",
+        mode: 'widthFix'
+      },
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20170922/20170922151756502731_original.jpg",
+        mode: 'widthFix'
+      },
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20170323/20170323155216955152_original.jpg",
+        mode: 'widthFix'
+      },
+      {
+        image:"https://shopstatic.vivo.com.cn/vivoshop/commodity/20170323/20170323155227379507_original.jpg",
+         mode: 'widthFix'
+      }
+      ,
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20170821/20170821083311616767_original.jpg",
+        mode: 'widthFix'
+      }
+      ,
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20170323/20170323155304684132_original.jpg",
+        mode: 'widthFix'
+      }
+      ,
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20170329/20170329154947686685_original.jpg",
+        mode: 'widthFix'
+      }
+      ,
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20170323/20170323155330211312_original.jpg",
+        mode: 'widthFix'
+      }
+      ,
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20170323/20170323155342477248_original.jpg",
+        mode: 'widthFix'
+      }
+      ,
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20171109/20171109160002274104_original.jpg",
+        mode: 'widthFix'
+      }
+      ,
+      {
+        image: "https://shopstatic.vivo.com.cn/vivoshop/commodity/20170629/20170629145248102954_original.jpg",
+        mode: 'widthFix'
+      }*/
+    ]
+  },
+]
+
+module.exports={
+  homeIndex:Home_data
+}
